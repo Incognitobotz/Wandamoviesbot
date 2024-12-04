a550f310dd935d4fb8db428a1ce11577114aee00
Incognitobotz <jaypalbarkesya@gmail.com>
2024-12-04 21:54:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,8 +77,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/JpOfficialSupport')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/jp_movies_update')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+WnJamUqcp_UwNDM1')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/+a0fP-zRSvwBlMzVl')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+WnaEIZmhq7AyMmU1')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/request_group_download")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/request_group_download")
 
